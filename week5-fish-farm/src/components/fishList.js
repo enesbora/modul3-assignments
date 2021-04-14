0cf18cdce44064f8f1e90d34a2dc849c2b94cc3f
enesbora <boraenes84@gmail.com>
2021-04-14 23:26:34 +0200

    fish-farm edited

--- week5-fish-farm/src/components/fishList.js
+++ week5-fish-farm/src/components/fishList.js
@@ -1,15 +1,14 @@
 /**
  * App js den gelen bilgilerinde map ile dönüyoruz ve her bir balik türünün bilgisini State kullanilarak asagiya aktariyoruz.
  */
- import {useState} from 'react'
- import Fish from './fish'
- 
- const FishList = (props) => {
-   const [fishList] = useState(props.fishList)
- return(
-    fishList.map((fish,index)=>
-   <Fish  key={index} fish={fish} index={index}/>
- ))
- }
+import { useState } from "react";
+import Fish from "./fish";
 
- export default FishList;
+const FishList = (props) => {
+  const [fishList] = useState(props.fishList);
+  return fishList.map((fish, index) => (
+    <Fish key={index} fish={fish} index={index} />
+  ));
+};
+
+export default FishList;
