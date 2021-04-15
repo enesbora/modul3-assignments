d00529c1128ed27de5531014bf8a8aa3b641adca
enesbora <boraenes84@gmail.com>
2021-04-15 18:14:10 +0200

    codes edited

--- week5-word-quiz/src/App.js
+++ week5-word-quiz/src/App.js
@@ -3,7 +3,6 @@
  *  Ekrana random olarak Almanca bir kelime ve 4 tane secenek gelecek. 
  * (Seceneklerden biri Almanca kelimenin dogru Türkce karsiligidir). 
  * Sorularin cevaplanmasiyla diger soruya gecilecek ve her soruya gecildiginde skor güncellenecek.
- * 
  * 1) Öncelikle bir json dosyasi olsuturulur. Olsuturulan dosyada sorulacak kelime ve cevaplar yer alir.
  * 2) QuizApp.js adinda oyunun caliscagi bir komponent olsuturulur. Burada oyun oynandigi zaman verilen cevaplarin dogru olup olmadigi state ile tutulur.
  * 3) Oyunun tamamlanip tamalanmadigi ilk olarak degerlendirilir.Eger oyunda Sorularin uzunluguna ulasilmasisa oyun devam eder (baslagic sorusu degeri ilk olarak satatüs ile 0 olarak tanimlandi.)
